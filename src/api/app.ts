465d71c9525f30e306aac8cf2337a12e70eba08c
Eduardo Izquierdo <sirmartin@gmail.com>
2023-04-02 12:21:05 +0200

    Created controller and routes.

--- src/api/app.ts
+++ src/api/app.ts
@@ -5,6 +5,7 @@ import lusca from "lusca";
 
 // Controllers (route handlers)
 import * as healthController from "./controllers/health";
+import * as geniallyController from "./controllers/genially";
 
 // Create Express server
 const app = express();
@@ -19,5 +20,9 @@ app.use(lusca.xssProtection(true));
 
 // Primary app routes
 app.get("/", healthController.check);
+app.post("/genially", geniallyController.createGenially)
+app.put("/genially/:id/rename", geniallyController.renameGenially);
+app.delete("/genially/:id", geniallyController.deleteGenially)
+
 
 export default app;
---
+++ src/api/controllers/genially.ts
@@ -0,0 +1,34 @@
+import { Response, Request } from "express";
+import CreateGeniallyService from "../../contexts/core/genially/application/CreateGeniallyService";
+import InMemoryGeniallyRepository from "../../contexts/core/genially/infrastructure/InMemoryGeniallyRepository";
+import RenameGeniallyService from "../../contexts/core/genially/application/RenameGeniallyService";
+import DeleteGeniallyService from "../../contexts/core/genially/application/DeleteGeniallyService";
+
+const repository = new InMemoryGeniallyRepository();
+
+export const createGenially = async (req: Request, res: Response) => {
+    const createGeniallyService = new CreateGeniallyService(repository)
+
+    const genially = await createGeniallyService.execute(req.body)
+
+    res.status(201).send(genially);
+};
+
+export const renameGenially = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const {newName} = req.body;
+    const renameGeniallyService = new RenameGeniallyService(repository);
+
+    const genially = await renameGeniallyService.execute(id, newName);
+
+    res.status(200).send(genially);
+};
+
+export const deleteGenially = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const deleteGeniallyService = new DeleteGeniallyService(repository);
+
+    await deleteGeniallyService.execute(id);
+
+    res.status(204).send();
+};
